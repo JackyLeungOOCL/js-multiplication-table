aadc7c1c4097e0aab68bdcd2a4ee8df153322c83
jacky.leung <jacky.leung@oocl.com>
2018-11-20 17:45:22 +0800

    create table element

--- multiply.js
+++ multiply.js
@@ -7,14 +7,30 @@ function inputIsValid(x, y) {
     if (x < 1 || y > 1000) {
         return false;
     }
-    
+
     return true;
+}
+
+function createTableElement(i, j) {
+    return i + "*" + j + "=" +  (i*j) + "";
 }
 
 function calculateAlignedMultiplicationTable(x, y) {
     var str = "";
 
-    
+    for (var j = x; j <= y; j++) {
+        for (var i = x; i <= j; i++) {
+            // console.log("i,j: " + i + "," + j);
+            let tmp = (i + "*" + j + "=" +  (i*j) + "");
+            // console.log(tmp);
+            // str.concat(tmp);
+            str = str + tmp;
+            // console.log();
+        }
+        str += "\n";
+    }
+
+    console.log(str);
     return str;
 }
 
